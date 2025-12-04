6145af6b12178bd4311ad950ea6a9e1bea5e03ff
jaeyeah <limjh819@naver.com>
2025-12-04 15:44:40 +0900

    회원가입 서버 연동 + axios/bootstrap import

--- final/finalproject/src/components/member/MemberJoin.jsx
+++ final/finalproject/src/components/member/MemberJoin.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import axios from "axios";
+import { useCallback, useMemo, useState } from "react";
 import { useNavigate } from "react-router-dom"
 
 
@@ -17,14 +18,46 @@ export default function member(){
         memberEmail : "", memberBirth : "", memberContact : "",
         memberPost : "", memberAddress1 : "", memberAddress2 : ""
     });
-    //effect
+
+    // callback
+    const changeStrValue = useCallback(e=>{
+        const {name, value} = e.target;
+        setMember(prev=>({...prev, [name]:value}))
+    },[])
+
+    const changeDateValue = useCallback((date)=>{
+        // → 별도의 포맷 전환 절차가 필요
+        const replacement = format(date,"yyyy-MM-dd");
+        setAccount(prev=>({...prev, accountBirth : replacement}));
+    },[]);
+
 
 
     //memo
-
+    // 모든 항목이 유효한지 검사(선택항목은 is-invalid가 아니어야함)
+    const memberValid = useMemo(()=>{
+        //필수항목
+        // if(memberClass.memberId !== "is-valid") return false;
+        // if(memberClass.memberPw !== "is-valid") return false;
+        // if(memberClass.memberNickname !== "is-valid") return false;
+        // if(memberClass.memberEmail !== "is-valid") return false;
+        // //선택항목
+        // if(memberClass.memberBirth==="is-invalid") return false;
+        // if(memberClass.memberContact==="is-invalid") return false;
+        // if(memberClass.memberPost==="is-invalid") return false;
+        // if(memberClass.memberAddress1==="is-invalid") return false;
+        // if(memberClass.memberAddress2==="is-invalid") return false;
+        // return true;
+    },[memberClass])
 
     //callback
-
+    //최종 가입
+    const sendData = useCallback(async()=>{
+        if(memberValid === false) return ;
+        const {data} = await axios.post("/member/",member)
+        
+        //navigate("/"); // 메인페이지
+    },[member,memberValid])
 
     //render
     return (<>
@@ -36,7 +69,7 @@ export default function member(){
             <div className="col-sm-9">
                 <input type="text" className={`form-control ${memberClass.memberId}`} 
                             name="memberId" value={member.memberId}
-                            //onChange={}
+                            onChange={changeStrValue}
                             //onBlur={}
                             />
                 <div className="valid-feedback"></div>
@@ -50,7 +83,7 @@ export default function member(){
             <div className="col-sm-9">
                 <input type="text" className={`form-control ${memberClass.memberPw}`} 
                             name="memberPw" value={member.memberPw}
-                            //onChange={}
+                            onChange={changeStrValue}
                             //onBlur={}
                             />
                 <div className="valid-feedback"></div>
@@ -64,7 +97,7 @@ export default function member(){
             <div className="col-sm-9">
                 <input type="text" className={`form-control ${memberClass.memberNickname}`} 
                             name="memberNickname" value={member.memberNickname}
-                            //onChange={}
+                            onChange={changeStrValue}
                             //onBlur={}
                             />
                 <div className="valid-feedback"></div>
@@ -78,7 +111,7 @@ export default function member(){
             <div className="col-sm-9">
                 <input type="text" className={`form-control ${memberClass.memberEmail}`} 
                             name="memberEmail" value={member.memberEmail}
-                            //onChange={}
+                            onChange={changeStrValue}
                             //onBlur={}
                             />
                 <div className="valid-feedback"></div>
@@ -92,7 +125,7 @@ export default function member(){
             <div className="col-sm-9">
                 <input type="text" className={`form-control ${memberClass.memberBirth}`} 
                             name="memberBirth" value={member.memberBirth}
-                            //onChange={}
+                            onChange={changeStrValue}
                             //onBlur={}
                             />
                 <div className="valid-feedback"></div>
@@ -106,7 +139,7 @@ export default function member(){
             <div className="col-sm-9">
                 <input type="text" className={`form-control ${memberClass.memberContact}`} 
                             name="memberContact" value={member.memberContact}
-                            //onChange={}
+                            onChange={changeDateValue}
                             //onBlur={}
                             />
                 <div className="valid-feedback"></div>
@@ -114,12 +147,15 @@ export default function member(){
             </div>
         </div>
         
+        {/* 주소 (Post, Address1, Address2) */}
+        {/* ---------------------------- */}
+        
         {/* 가입버튼  */}
         <div className="row mt-4">
             <div className="col">
                 <button type="button" className="btn btn-lg btn-success w-100"
-                            //disabled={accountValid === false}
-                            //onClink = {sendData}
+                            disabled={memberValid === false}
+                            onClick = {sendData}
                             >
                 <span>가입</span>
                 </button>
