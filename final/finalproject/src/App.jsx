404a22f0a99fdb74f3532ff9da14c31876712972
jaeyeah <limjh819@naver.com>
2025-12-06 23:05:19 +0900

    임시 메뉴

--- final/finalproject/src/App.jsx
+++ final/finalproject/src/App.jsx
@@ -2,6 +2,7 @@
 import './App.css'
 import { BrowserRouter } from "react-router-dom"
 import Content from "./components/Content"
+import Menu from './components/Menu'
 
 function App() {
 
@@ -9,9 +10,10 @@ function App() {
     <>
       {/* Router는 주소에 의한 화면 분할을 처리하는 도구이며 설정된 영역 내에서만 작동함 */}
       <BrowserRouter>
-        <div className="container-fluid my-5 pt-5">
-          <Content/>
-        </div>
+        <Menu/>
+          <div className="container-fluid my-5 pt-5">
+            <Content/>
+          </div>
       </BrowserRouter>
     </>
   )
---
+++ final/finalproject/src/components/Menu.jsx
@@ -0,0 +1,28 @@
+import { useAtom, useAtomValue, useSetAtom } from "jotai";
+import { useNavigate } from "react-router-dom"
+import { accessTokenState, adminState, clearLoginState, loginIdState, loginLevelState, loginState } from "../utils/jotai";
+
+
+
+
+export default function Menu() {
+    const navigate = useNavigate();
+
+    //통합 state
+    const [loginState, setLoginState] = useAtom(loginIdState);
+    const [loginLevel, setLoginLevel] = useAtom(loginLevelState);
+    const [loginComplete, setLoginComplete] = useAtom(loginState); 
+    const [accessToken, setAccessToken] = useAtom(accessTokenState);
+    const isLogin = useAtomValue(loginState);
+    const isAdmin = useAtomValue(adminState);  
+    const clearLogin = useSetAtom(clearLoginState);
+
+    return(<>
+        <div className="row mt-4">
+            <div className="col">
+                <h1>임시 메뉴 화면</h1>
+                <Link to="/">홈</Link> |    
+            </div>
+        </div>
+    </>)
+}
