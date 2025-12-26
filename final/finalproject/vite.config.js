5848fca19ad89c126e300f45031fb6f524b05906
jaeyeah <limjh819@naver.com>
2025-12-26 13:54:14 +0900

    배포용 경로 수정 및 axios 기본 URL 환경변수화

--- final/finalproject/vite.config.js
+++ final/finalproject/vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { loadEnv } from 'vite'
-// https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
+
 
 export default defineConfig(({mode})=>{
   // mode에 맞는 환경 정보를 불러오도록 코드 작성
   const env = loadEnv(mode,process.cwd());
-  const baseUrl = env.VITE_BASE_URL;
+  // const baseUrl = env.VITE_BASE_URL;
+  const baseUrl = env.VITE_BASE_PATH;
   return {
-    SERVER : {
+    server : {
       host : "0.0.0.0",
       port : 5173
     },
